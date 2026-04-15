251bf5b918c9fbb003399688af714d076a2de542
Lazaro-Nascimento <lazaronascimentogms@gmail.com>
2026-04-14 22:05:22 -0300

    [feat] - Criado estruturas (rosto_t, sorriso_t, olho_t) e função DesenharFormatoPPM para implementar o desenho no arquivo .ppm

--- Computação Grafica/smileyface.cpp
+++ Computação Grafica/smileyface.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <cmath> // Necessário para sqrt() e pow()
-
+#define RAIO 0.3f
 
 struct olho_t{
     float centroX;
     float centroY;
+    float distancia;
 };
+
+struct sorriso_t{
+    float distancia;
+    float centroX;
+    float centroY;
+};
+
+struct rosto_t{
+    float distancia;
+    float centroX;
+    float centroY;
+};
+
+void DesenharFormatoPPM(rosto_t rosto, olho_t olhoEsquerdo, olho_t olhoDireito, sorriso_t sorriso, std::ofstream& arquivo, int dim);
+
 int main() {
+
+    rosto_t rosto;
 
     olho_t olhoEsquerdo, olhoDireito;
 
-    const int dim = 400;
+    sorriso_t sorriso;
 
-    float centro_x = 0.5f, centro_y = 0.5f, raio = 0.3f, distanciaRosto, distanciaOlhoEsquerdo, distanciaOlhoDireito, distanciaSorriso;
+    int dim = 400;
 
-    float alturaDosOlhos = (float)(0.5 - raio/2);
+    float alturaDosOlhos, posicaoXOlhoDireito, posicaoXOlhoEsquerdo;
 
-    float posicaoXOlhoDireito = (float)(0.5 - raio + raio + raio/2.0);
 
-    float posicaoXOlhoEsquerdo = (float)(0.5 - raio/2);
+    alturaDosOlhos = (float)(0.5 - RAIO/2);
+
+    posicaoXOlhoDireito = (float)(0.5 - RAIO + RAIO + RAIO/2.0);
+
+    posicaoXOlhoEsquerdo = (float)(0.5 - RAIO/2);
+
+    rosto.centroX = 0.5f;
+    rosto.centroY = 0.5f;
+
+    sorriso.centroX = 0.5f;
+    sorriso.centroY = 0.5f;
 
     olhoDireito.centroX = posicaoXOlhoDireito;
     olhoDireito.centroY = alturaDosOlhos;
@@ -27,49 +54,56 @@ int main() {
     olhoEsquerdo.centroX = posicaoXOlhoEsquerdo;
     olhoEsquerdo.centroY = alturaDosOlhos;
 
-    std::ofstream arquivo("/mnt/c/Users/lazar/Downloads/circulo.ppm");
+    std::ofstream arquivo("/mnt/c/Users/lazar/Downloads/smileyface.ppm");
 
     arquivo << "P3\n" << dim << " " << dim << "\n255\n";
 
-    for (int y = 0; y < dim; ++y) {
+    DesenharFormatoPPM(rosto, olhoEsquerdo, olhoDireito, sorriso, arquivo, dim);
+
+    arquivo.close();
+    return 0;
+}
+
+void DesenharFormatoPPM(rosto_t rosto, olho_t olhoEsquerdo, olho_t olhoDireito, sorriso_t sorriso, std::ofstream& arquivo, int dim)
+{
+
+     for (int y = 0; y < dim; ++y) {
+
         for (int x = 0; x < dim; ++x) {
-            // 1. Normalizar as coordenadas (0.0 a 1.0)
+
             float u = (float)x / dim;
             float v = (float)y / dim;
 
-            // d = sqrt( (x2-x1)^2 + (y2-y1)^2 )
-            distanciaRosto = std::sqrt(std::pow(u - centro_x, 2) + std::pow(v - centro_y, 2));
+            rosto.distancia = std::sqrt(std::pow(u - rosto.centroX, 2) + std::pow(v - rosto.centroY, 2));
 
-	    distanciaOlhoEsquerdo = std::sqrt(std::pow(u - olhoEsquerdo.centroX, 2) + std::pow(v - olhoEsquerdo.centroY, 2));
+	    olhoEsquerdo.distancia = std::sqrt(std::pow(u - olhoEsquerdo.centroX, 2) + std::pow(v - olhoEsquerdo.centroY, 2));
 
-	    distanciaOlhoDireito = std::sqrt(std::pow(u - olhoDireito.centroX, 2) + std::pow(v - olhoDireito.centroY, 2));
+	    olhoDireito.distancia = std::sqrt(std::pow(u - olhoDireito.centroX, 2) + std::pow(v - olhoDireito.centroY, 2));
 
-	    distanciaSorriso = std::sqrt(std::pow(u - centro_x, 2) + std::pow(v - centro_y,2));
+	    sorriso.distancia = std::sqrt(std::pow(u - sorriso.centroX, 2) + std::pow(v - sorriso.centroY,2));
 
-	    if (distanciaOlhoDireito < raio/5)
+	    if (olhoDireito.distancia < RAIO/5)
 	    {
 		arquivo << "0 0 0\n";
 	    }
 
-	    else if (distanciaOlhoEsquerdo < raio/5)
+	    else if (olhoEsquerdo.distancia < RAIO/5)
             {
                 arquivo << "0 0 0\n";
             }
 
-	    else if (distanciaSorriso < raio/2 && v > centro_y && distanciaSorriso > raio/3)
+	    else if (sorriso.distancia < RAIO/2 && v > rosto.centroY && sorriso.distancia > RAIO/3)
 	    {
 		arquivo << "0 0 0\n";
 	    }
 
-	    else if (distanciaRosto < raio) {
-                arquivo << "255 255 0\n"; // Branco (dentro do círculo)
+	    else if (rosto.distancia < RAIO) {
+                arquivo << "255 255 0\n";
             }
 
 	    else {
-                arquivo << "0 0 0\n";       // Preto (fundo)
+                arquivo << "0 0 0\n";
             }
         }
      }
-    arquivo.close();
-    return 0;
 }
